05dc4f4391845dd8f938b30d08e53cf274da1185
AtifNaeem1 <100839831+AtifNaeem1@users.noreply.github.com>
2022-07-18 15:32:08 +0530

    Delete Bridge directory

--- Bridge/Produce.java
+++
@@ -1,9 +0,0 @@
-package com.Bridge;
-
-class Produce implements Workshop {
-    @Override
-    public void work()
-    {
-        System.out.print("Produced");
-    }
-}
--- Bridge/Workshop.java
+++
@@ -1,5 +0,0 @@
-package com.Bridge;
-
-public interface Workshop {
-    abstract public void work();
-}
